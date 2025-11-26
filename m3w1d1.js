7609c3f5805ce56e02d5926b5f126a1132aee0e1
Luciano Vaccaro <luciano.vaccaro510@gmail.com>
2025-11-26 17:09:05 +0100

    completati gli esercizi extra 2 e 3

--- m3w1d1.js
+++ m3w1d1.js
@@ -5,3 +5,29 @@ function rimuovere() {
   }
 }
 rimuovere();
+
+function remove() {
+  let contLeggere = document.querySelector(".jumbotron a");
+  if (contLeggere) {
+    contLeggere.addEventListener("click", function (e) {
+      e.preventDefault("a");
+      let genitore = e.target.closest(".jumbotron");
+      if (genitore) {
+        genitore.remove();
+      }
+    });
+  }
+}
+remove();
+
+function gestisciAutori() {
+  let autori = document.querySelectorAll(".blog-post-meta a");
+
+  for (let i = 0; i < autori.length; i++) {
+    autori[i].addEventListener("mouseover", function (e) {
+      let nomeAutore = e.target.textContent;
+      alert("Autore del post : " + nomeAutore);
+    });
+  }
+}
+gestisciAutori();
